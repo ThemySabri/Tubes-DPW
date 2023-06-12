bd9f08a3e576f365cd9911b3e5e95bfc92a9b56e
Nabil Hanif <nabiltitimplik@gmail.com>
2023-06-12 22:13:33 +0700

    fix

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/Kuliah/tubes-dpw';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = "";
 
 /*
 |--------------------------------------------------------------------------
